349266b272a8ad5a53dc49142d659fde4a57045e
ProcyonB <pelagea.nikolaevna@gmail.com>
2020-09-14 20:27:27 +0300

    form+about

--- js/script.js
+++ js/script.js
@@ -4,3 +4,24 @@ $(".slider").slick({
   arrows: true,
   swipeToSlide: true,
 });
+new WOW().init();
+
+$(document).on("click", ".logo", function () {
+  var linkID = $(this).attr("href");
+  $("html, body").animate(
+    {
+      scrollTop: $(linkID).offset().top,
+    },
+    "slow"
+  );
+  return false;
+});
+
+$(".cont_btn").on("click", function () {
+  $(".popup").fadeIn();
+  return false;
+});
+$(".close").on("click", function () {
+  $(".popup").fadeOut();
+  return false;
+});
